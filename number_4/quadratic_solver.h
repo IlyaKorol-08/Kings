c4ba6f41a5d4167126e3903e22cdf9dc40c6c3bb
IlyaKorol-08 <iliakorol2008@gmail.com>
2026-03-27 00:44:54 +0600

    Update quadratic_solver.h

--- number_4/quadratic_solver.h
+++ number_4/quadratic_solver.h
@@ -1,6 +1,6 @@
 #ifndef QUADRATIC_SOLVER_H
 #define QUADRATIC_SOLVER_H
 
-int solve_quadratic(double a, double b, double c, double *roots);
+void solve_quadratic(double a, double b, double c, double *roots);
 
 #endif /* QUADRATIC_SOLVER_H */
